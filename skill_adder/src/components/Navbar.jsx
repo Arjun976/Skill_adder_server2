7206dd34b3de14fa0e96be1e8ea7e9e8642c77b5
AK <aakhilp1@gmail.com>
2025-10-29 19:42:33 +0530

    create a card and applying styling

---
+++ skill_adder/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
