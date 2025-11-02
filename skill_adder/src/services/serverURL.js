e39cc2ae66398ff32760c46f7b189ad64d6ea208
Arjun976 <arjunanil508@gmail.com>
2025-11-02 14:03:49 +0530

    fix:server hosted

--- skill_adder/src/services/serverURL.js
+++ skill_adder/src/services/serverURL.js
@@ -1,3 +1,3 @@
-  export const serverURL = 'http://localhost:3000'
+//   export const serverURL = 'http://localhost:3000'
 
-// export const serverURL='https://skill-adder-server2-17.onrender.com'
+export const serverURL='https://skill-adder-server2-17.onrender.com'
